ff391808fc9dfe8a2e8a502337ce9c92565c463b
Zachary Ernst <zernst@narrativescience.com>
2014-09-17 11:06:38 -0500

    initial commit for import file

--- global_config.py
+++ global_config.py
@@ -9,3 +9,9 @@ DATABASE = 'GratefulDeadConcerts'
 PASSWORD = 'my_super_secret_password_from_the_orientdb_config_file'
 LANGUAGE = 'sql'
 USER = 'root'
+BASE_DIR = '/Users/zernst/projects/py2orientdb'
+TTL_DIR = BASE_DIR + '/' + 'ttl'
+ARTICLE_CATEGORIES_FILE = TTL_DIR + '/' + 'article_categories_en.ttl.gz'
+
+#article_categories_en.ttl.gz labels_en.ttl.gz
+#geo_coordinates_en.ttl.gz    persondata_en.ttl.gz
